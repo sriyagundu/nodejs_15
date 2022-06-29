4fbbfbabaf5ad9d482aeda7442ce679118d8c330
sriyagundu <99172667+sriyagundu@users.noreply.github.com>
2022-06-29 18:10:16 +0530

    Add files via upload

---
+++ exp 17.js
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body link="green" alink="yellow" vlink="red">
+	<center>
+		<h1>LAB EXPERIMENTS</h1>
+		<form>
+			<table border="1" cellpadding="3" cellspacing="0" width="400" >
+				<tr>
+					<td>S.NO</td>
+					<td>Experiment Name</td>
+					<td>Experiment Link</td>
+				</tr>
+				<tr align="center"><td colspan="3">Experiment - 1</td></tr>
+				<tr>
+					<td>1</td>
+					<td>Basic Html tags</td>
+					<td><a href=1_a.html target="_blank">exp1_a</a></td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Text formatting tags in Html</td>
+					<td><a href=1_b.html target="_blank">exp1_b</a></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Hyper links</td>
+					<td><a href=1_c.html target="_blank">exp1_c</a></td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Marquee Tags</td>
+					<td><a href=1_d.html target="_blank">exp1_d</a></td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Image tag</td>
+					<td><a href=1_e.html target="_blank">exp1_e</a></td>
+				</tr>
+				<tr>
+					<td>6</td>
+					<td>Lists</td>
+					<td><a href=1_f.html target="_blank">exp1_f</a></td>
+				</tr>
+				<tr>
+					<td>7</td>
+					<td>Table</td>
+					<td><a href=1_g.html target="_blank">exp1_f</a></td>
+				</tr>
+				<tr>
+					<td>8</td>
+					<td>Resume</td>
+					<td><a href=1_h.html target="_blank">exp1_f</a></td>
+				</tr>
+				<tr>
+					<td>9</td>
+					<td>Registration from</td>
+					<td><a href=1_i.html target="_blank">exp1_i</a></td>
+				</tr>
+				<tr>
+					<td>10</td>
+					<td>Login form</td>
+					<td><a href=1_j.html target="_blank">exp1_j</a></td>
+				</tr>
+				<tr>
+					<td>11</td>
+					<td>Forgot password</td>
+					<td><a href=1_k.html target="_blank">exp1_k</a></td>
+				</tr>
+				<tr align="center"><td colspan="3">Experiment - 2</td></tr>
+				<tr>
+					<td>1</td>
+					<td>External file<br>
+						Embedded file<br>
+						Inline file
+					</td>
+					<td><a href=2_a.html target="_blank">exp2_a</a><br>
+						<a href=2_b.html target="_blank">exp2_b</a><br>
+						<a href=2_c.html target="_blank">exp2_c</a>
+					</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>css  text formattings</td>
+					<td><a href=2_d.html target="_blank">exp2_d</a></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>css border tags</td>
+					<td><a href=2_e.html target="_blank">exp2_e</a></td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>css margin and paddding tags</td>
+					<td><a href=2_f.html target="_blank">exp2_f</a></td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>css box model example</td>
+					<td><a href=2_g.html target="_blank">exp2_g</a></td>
+				</tr>
+				<tr>
+					<td>6</td>
+					<td>Resume with css</td>
+					<td><a href=2_h.html target="_blank">exp2_h</a></td>
+				</tr>
+				<tr>
+					<td>7</td>
+					<td>CSS Selectors</td>
+					<td><a href=2_i.html target="_blank">exp2_i</a></td>
+				</tr>
+				<tr>
+					<td>8</td>
+					<td>Links with css</td>
+					<td><a href=2_j.html target="_blank">exp2_j</a></td>
+				</tr>
+				<tr>
+					<td>9</td>
+					<td>Lists with css</td>
+					<td><a href=2_k.html target="_blank">exp2_k</a></td>
+				</tr>
+				<tr>
+					<td>10</td>
+					<td>Psuedo elements</td>
+					<td><a href=2_l.html target="_blank">exp2_l</a></td>
+				</tr>
+				<tr align="center"><td colspan="3">Experiment - 3</td></tr>
+				<tr>
+					<td>1</td>
+					<td>Alert and prompt using javascript</td>
+					<td><a href=sample.html target="_blank">exp1_a</a></td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Alert and prompt using js in exteral file</td>
+					<td><a href=sample_external.html target="_blank">exp1_b</a></td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Squares and cubes upto 10 using js</td>
+					<td><a href=squares_cubes.html target="_blank">exp1_c</a></td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Buttons using js</td>
+					<td><a href=basic_js.html target="_blank">exp1_d</a></td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Factorial, palindrome, Fibonacci, primes using js</td>
+					<td><a href=buttons_4.html target="_blank">exp1_e</a></td>
+				</tr>
+			</table>
+		</form>
+		<br>
+		<font align = "right">G sai sriya<br>20B01A5415</font>
+	</center>
+</body>
+</html>
